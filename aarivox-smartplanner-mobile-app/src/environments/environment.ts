4eb0a74b7471dc9e2e542a8760b782ba4ec95078
vinod <sanduvinod9052@gmail.com>
2026-06-26 12:11:06 +0530

    Initial commit

--- aarivox-smartplanner-mobile-app/src/environments/environment.ts
+++ aarivox-smartplanner-mobile-app/src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api',
+  // apiUrl: 'http://localhost:8080/api',
+  apiUrl: 'http://13.62.111.155/',
   firebase: {
     apiKey: "YOUR_API_KEY",
     authDomain: "YOUR_AUTH_DOMAIN",
